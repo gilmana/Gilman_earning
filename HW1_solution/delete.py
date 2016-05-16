1c471eeb67c56b8cbecae843bc5bd99bc4bd8371
Alexey Gilman <gilmana@uw.edu>
2016-05-16 07:44:25 -0700

    cleaning up HW directory

--- HW1_solution/delete.py
+++
@@ -1,3 +0,0 @@
-print("this is python 3.5")
-
-print "this is python 2.7"
--- dacb.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env python
-
-import pandas as pd
-
-df=pd.read_csv("output.txt")
-print(df.sample(n=10, replace=True))
