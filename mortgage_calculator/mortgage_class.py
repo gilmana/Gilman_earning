fa1f04eb5a997309c482a00b450b453f4af8383e
Alexey Gilman <gilmanalexey@gmail.com>
2021-07-05 15:51:52 -0700

    improvving mortgage class

--- mortgage_calculator/mortgage_class.py
+++ mortgage_calculator/mortgage_class.py
@@ -1,36 +1,60 @@
 import numpy as np
 import pandas as pd
 
-class Loan:
-    def __init__(self, principle, rate, duration):
-        self.principle = principle
-        self.rate = rate/12/100  # convert annual percent into monthly decimal percent
-        self.duration = duration*12  # convert duration in years into months
 
-    def monthly(self):
-        p = self.principle
-        r = self.rate
-        n = self.duration
+class Loan:
+    """
+    Stores parameters about a single loan.
+    """
+    def __init__(self):
+        self.loan = {}
+
+    def input_loan(self):
+        """
+        calls user input for three
+
+        :return: None
+        """
+        # data inputs
+        principle = float(input("Enter principle amount: "))
+        rate = float(input("Enter interest rate as %: "))
+        duration = int(input("Enter loan duration in years: "))
+
+        # calculating monthly payment
+        p = principle
+        r = rate / 12 / 100
+        n = duration * 12
 
         numerator = r * p * ((1 + r) ** n)
         denomenator = ((1 + r) ** n) - 1
+        monthly = numerator / denomenator
 
-        return(numerator/denomenator)
+        # assigning values to attributes
+        self.loan["principle"] = principle
+        self.loan["rate"] = rate
+        self.loan["duration"] = duration
+        self.loan["monthly"] = monthly
 
-    def describe(self):
-        print(f"Loan principle: {self.principle}")
-        print(f"Loan annual interest (%) {self.rate*12*100}")
-        print(f"Loan duration (Month) {self.duration}")
-        print(f"Monthly payment ($) {self.monthly()}")
 
-    def schedule(self):
-        """
-        Plot the amortization schedule of the loan class
-        """
-        m = self.monthly()  # monthly payment
-        count = np.arange(1, self.duration + 1)  # array with payment counts
-        paid = count * m  # array with total paid for every payment
-        array = np.stack([count, paid], axis=1)
-        df = pd.DataFrame(array, columns=["Months", "Paid"])
+class Portfolio:
+    """
+    Creating a portfolio of loans. Utilization of Loan class to add new loans to portfolio.
+    """
+    def __init__(self):
+        self.portfolio = {}
 
-        return (df.set_index("Months").plot())
+    def add_loan(self, loan_id=None):
+
+        if loan_id == None:
+            loan_id = input("Enter loan ID: ")
+        else:
+            pass
+
+        loan_dict = Loan()
+        loan_dict.input_loan()
+
+        self.portfolio[str(loan_id)] = loan_dict.loan
+
+
+x = 5
+print(y)
